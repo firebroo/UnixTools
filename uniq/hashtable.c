eec59339578f7b6b4ee1136757d008b105c11434
root <root@10.0.2.15>
2018-07-29 17:43:00 +0800

    #连续数组版本

--- uniq/hashtable.c
+++ uniq/hashtable.c
@@ -1,12 +1,13 @@
 #include "hashtable.h"
 
+
 /* initialize hash table */
 void
 hash_table_init (HashTable *hashtable)
 {
     hashtable->hash_table_max_size = HASH_TABLE_MAX_SIZE;
     hashtable->hash_size = 0;
-    hashtable->hashnode = (HashNode **)calloc (sizeof(HashNode*), \
+    hashtable->hashnode = (HashNode *)calloc (sizeof(HashNode), \
             HASH_TABLE_MAX_SIZE);
 }
 
@@ -14,17 +15,18 @@ HashNode*
 create_new_node (const char *skey) {
     HashNode    *pNewNode;
 
-    pNewNode = (HashNode*) malloc (sizeof (HashNode) + strlen(skey)) ;
+    pNewNode = (HashNode*) malloc (sizeof (HashNode)); 
+    pNewNode->sKey = mallocStr(skey);
     strcpy (pNewNode->sKey, skey);
     return pNewNode;
 }
 
 
-inline unsigned long 
+ unsigned long 
 hash_func (const char *arKey)
 {
     unsigned int nKeyLength = strlen(arKey);
-	register unsigned long hash = 5381;
+	register unsigned long hash = 0;
 
 	/* variant with the hash unrolled eight times */
 	for (; nKeyLength >= 8; nKeyLength -= 8) {
@@ -53,25 +55,25 @@ hash_func (const char *arKey)
 
 /* string hash function */
 /*
-unsigned int
-hash_table_hash_str (const char* skey)
-{
-    const char   *p;
-    unsigned int  hash;
-
-    p = (const char*)skey;
-    hash = *p;
-    if (hash) {
-        for (p += 1; *p != '\0'; p++)
-            hash = (hash << 5) - hash + *p;
-    }
-    return hash;
-}
-*/
+ * unsigned int
+ * hash_table_hash_str (const char* skey)
+ * {
+ *     const char   *p;
+ *         unsigned int  hash;
+ *
+ *             p = (const char*)skey;
+ *                 hash = *p;
+ *                     if (hash) {
+ *                             for (p += 1; *p != '\0'; p++)
+ *                                         hash = (hash << 5) - hash + *p;
+ *                                             }
+ *                                                 return hash;
+ *                                                 }
+ *                                                 */
 
 /*insert key-value into hash table, if key is exist, 
- *it will overwrite old value, use link list to slove 
- *hash conflict,*/
+ *  *it will overwrite old value, use link list to slove 
+ *   *hash conflict,*/
 bool
 hash_table_insert_str (HashTable *hashtable, const char* skey)
 {
@@ -80,10 +82,10 @@ hash_table_insert_str (HashTable *hashtable, const char* skey)
     HashNode       *pLast;
     HashNode       *pNewNode;
 
-    pos = hash_pos (skey);
-    pHead = (hashtable->hashnode)[pos];
+    pos = hash_func((skey)) % HASH_TABLE_MAX_SIZE;
+    pHead = hashtable->hashnode + pos;
 
-    if (pHead) {
+    if (pHead->sKey) {
         do {
             if (strcmp (pHead->sKey, skey) == 0) {  
                 return false; 
@@ -93,28 +95,9 @@ hash_table_insert_str (HashTable *hashtable, const char* skey)
         pNewNode = create_new_node(skey);
         pLast->pNext = pNewNode;
     } else {
-        pNewNode = create_new_node(skey);
-        (hashtable->hashnode)[pos] = pNewNode;
+        pHead->sKey = mallocStr(skey);
+        strcpy (pHead->sKey, skey);
     }
     return true;
 }
 
-/*free the memory of the hash table*/
-void
-hash_table_release (HashTable *hashtable)
-{
-    size_t      i;
-    HashNode   *pHead, *pTemp;
-
-    for (i = 0; i < hashtable->hash_table_max_size; i++) {
-        if ((pHead = (hashtable->hashnode)[i])) {
-            while (pHead) {
-                pTemp = pHead;
-                pHead = pHead->pNext;
-                free (pTemp->sKey);
-                free (pTemp);
-            }
-        }
-    }
-    free (hashtable->hashnode);
-}
